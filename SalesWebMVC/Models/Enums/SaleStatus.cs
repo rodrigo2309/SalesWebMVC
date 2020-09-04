be65634db9ec0f1dfdcf5bf1f1e64b9f902c71b9
rodrigo.mori <rodrigo.mori@imusica.com.br>
2020-09-03 23:43:25 -0300

    Project created

---
+++ SalesWebMVC/Models/Enums/SaleStatus.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SalesWebMVC.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        Peding = 0,
+        Billed = 1,
+        Canceled =2
+    }
+}
---
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -0,0 +1,29 @@
+﻿using SalesWebMVC.Models.Enums;
+using System;
+
+
+namespace SalesWebMVC.Models
+{
+    public class SalesRecord
+    {
+        public int id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller{ get; set; }
+
+        public SalesRecord()
+        {
+
+        }
+
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        {
+            this.id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
